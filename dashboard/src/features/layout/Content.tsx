f9b465feda81986f5396e129b5dfa61f3b90385e
Eirik Stenersen Sand <eiriksand@gmail.com>
2021-02-18 12:37:59 +0100

    Main content scaffolding

--- dashboard/src/features/layout/Content.tsx
+++ dashboard/src/features/layout/Content.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { CContainer, CFade } from '@coreui/react'
+
+const loading = (
+    <div className="pt-3 text-center">
+        <div className="sk-spinner sk-spinner-pulse"></div>
+    </div>
+)
 
 const Content = () => {
     return (
-        <div>Content</div>
+        <main className="c-main">
+            <CContainer fluid>
+                <Suspense fallback={loading}>
+                    Main Content
+                </Suspense>
+            </CContainer>
+        </main>
     )
 }
 
