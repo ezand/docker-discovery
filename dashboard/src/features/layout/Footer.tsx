666672deb3cc2caaa22abe6791d96a1ed4919971
Eirik Stenersen Sand <eiriksand@gmail.com>
2021-02-18 12:35:46 +0100

    Footer scaffolding

--- dashboard/src/features/layout/Footer.tsx
+++ dashboard/src/features/layout/Footer.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { CFooter } from '@coreui/react'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <CFooter fixed={false}>
+            <div>
+                <a href="https://github.com/ezand/docker-discovery" target="_blank" rel="noopener noreferrer">Docker Discovery</a>
+                <span className="ml-1">&copy; 2021 Eirik Stenersen Sand</span>
+            </div>
+            <div className="mfs-auto">
+                <span className="mr-1">Powered by</span>
+                <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">CoreUI</a>
+            </div>
+        </CFooter>
     )
 }
 
